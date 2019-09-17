a63818db96d3b33659c9e2bda2941e011559e9f6
Marcos-Pimentel <marcos.salles@estudante.ufla.br>
2019-09-16 21:23:44 -0300

    Criado o Método MA3()

--- A.cpp
+++ A.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
 class A{
-	private:
-		int A1;
-		float A2;
-	
-	public:
-		int getA1(){return A1;}
-		float getA2(){return A2;}
-		void setA1(int a){A1 = a;}
-		void setA2(int a){A2 = a;}
-		void MA1(){cout << "MA1" << endl;}
-		void MA2(){cout << "MA2" << endl;}
+private:
+	int A1;
+	float A2;
+
+public:
+	int getA1(){return A1;}
+	float getA2(){return A2;}
+	void setA1(int a){A1 = a;}
+	void setA2(int a){A2 = a;}
+	void MA1(){cout << "MA1" << endl;}
+	void MA2(){cout << "MA2" << endl;}
+	void MA3(){cout << "Alteração a classe A partir do clone" << endl;}
 };
