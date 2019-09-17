08a473f4340001909b116cc3428d4049edc404ea
Marcos-Pimentel <54404493+Marcos-Pimentel@users.noreply.github.com>
2019-09-16 21:06:10 -0300

    Add files via upload

---
+++ A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+class A{
+	private:
+		int A1;
+		float A2;
+	
+	public:
+		int getA1(){return A1;}
+		float getA2(){return A2;}
+		void setA1(int a){A1 = a;}
+		void setA2(int a){A2 = a;}
+		void MA1(){cout << "MA1" << endl;}
+		void MA2(){cout << "MA2" << endl;}
+};
---
+++ B.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+class B{
+	private:
+		int B1;
+		float B2;
+	
+	public:
+		int getB1(){return B1;}
+		float getB2(){return B2;}
+		void setB1(int a){B1 = a;}
+		void setB2(int a){B2 = a;}
+		void MB1(){cout << "MB1" << endl;}
+		void MB2(){cout << "MB2" << endl;}
+};
